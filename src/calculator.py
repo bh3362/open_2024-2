d17e8e0f9ef1aced4230a75bab16da8968fec30d
bh3362 <163098881+bh3362@users.noreply.github.com>
2024-10-18 19:28:29 +0900

    Update calculator.py
    수정1

--- src/calculator.py
+++ src/calculator.py
@@ -3,7 +3,7 @@
 operator = input("연산을 선택하세요 (+, -, *, /): ")
 num1 = float(input("첫 번째 숫자를 입력하세요: "))
 num2 = float(input("두 번째 숫자를 입력하세요: "))
-
+print("수정")
 if operator == '+':
     print(f"{num1} + {num2} = {num1 + num2}")
 elif operator == '-':
@@ -17,3 +17,5 @@ elif operator == '/':
         print("0으로 나눌 수 없습니다.")
 else:
     print("잘못된 연산자입니다.")
+
+
