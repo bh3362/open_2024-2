af81053be6ec2b6624d8dbbc868cc3d1b1338c51
bh3362 <163098881+bh3362@users.noreply.github.com>
2024-11-08 17:29:28 +0900

    Update ex3-3.sh

--- src/ex3-3.sh
+++ src/ex3-3.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# 입력 인수 할당
+
 weight=$1
 height=$2
 
-# BMI 계산 (BMI = 몸무게 / (신장 * 신장))
+# BMI 계산
 bmi=$(echo "scale=2; $weight / (($height / 100) * ($height / 100))" | bc)
 
 # 비만 여부 판정
